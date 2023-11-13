b69c30392fac5a5f349e10710296e760d8f68749
maksud9909 <rmaksud822@gmail.com>
2023-11-13 15:38:15 +0500

    Learning of Spring

---
+++ src/main/java/spring_intro/Dog.java
@@ -0,0 +1,13 @@
+package spring_intro;
+
+public class Dog implements Pet{
+    @Override
+    public void say(){
+        System.out.println("Bow-Wow");
+
+    }
+}
+
+interface Pet {
+    public void say();
+}

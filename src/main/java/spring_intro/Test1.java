6ff7e9f39738704ef90c82789c9c24b0c7d62159
maksud9909 <rmaksud822@gmail.com>
2023-11-13 16:16:44 +0500

    Update

--- src/main/java/spring_intro/Test1.java
+++ src/main/java/spring_intro/Test1.java
@@ -1,9 +1,14 @@
 package spring_intro;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test1 {
     public static void main(String[] args) {
-        Pet pet = new Cat();
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("MyPet",Pet.class);
         pet.say();
+        context.close();
     }
 }
 

2d6e8ba181cb31594db003c26439f206ff95a979
maksud9909 <rmaksud822@gmail.com>
2023-11-17 15:30:15 +0500

    Learning of Spring

---
+++ src/main/java/spring_intro/Person.java
@@ -0,0 +1,15 @@
+package spring_intro;
+
+public class Person {
+    private Pet pet;
+
+    public Person(Pet pet) {
+        this.pet = pet;
+    }
+
+
+    public void callYourPet(){
+        System.out.println("Hello my Pet");
+        pet.say();
+    }
+}
---
+++ src/main/java/spring_intro/Test3.java
@@ -0,0 +1,13 @@
+package spring_intro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+
+
+
+        context.close();
+    }
+}
